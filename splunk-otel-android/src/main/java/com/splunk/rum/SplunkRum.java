cbc45a8356b7c800f05596850e0d4e0eaf6673c2
jkwatson <jwatson@splunk.com>
2021-05-17 12:32:40 -0700

    Add some javadoc.

--- splunk-otel-android/src/main/java/com/splunk/rum/SplunkRum.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/SplunkRum.java
@@ -2,6 +2,9 @@ package com.splunk.rum;
 
 import android.util.Log;
 
+/**
+ * Entrypoint for Splunk's Android RUM (Real User Monitoring) support.
+ */
 public class SplunkRum {
 
     private static final String LOG_TAG = "SplunkRum";
@@ -14,10 +17,21 @@ public class SplunkRum {
         this.config = config;
     }
 
+    /**
+     * Create a new {@link Config.Builder} instance.
+     */
     public static Config.Builder newConfigBuilder() {
         return Config.builder();
     }
 
+    /**
+     * Initialized the Splunk RUM library with the provided {@link Config} instance.
+     * Note: if you call this method more than once, only the first one will do anything. Repeated
+     * calls will just immediately return the previously configured instance.
+     *
+     * @param config The {@link Config} options to use for initialization.
+     * @return A fully initialized {@link SplunkRum} instance, ready for use.
+     */
     public static SplunkRum initialize(Config config) {
         if (INSTANCE != null) {
             Log.w(LOG_TAG, "Singleton SplunkRum instance has already been initialized.");
@@ -27,6 +41,9 @@ public class SplunkRum {
         return INSTANCE;
     }
 
+    /**
+     * Get the singleton instance of this class.
+     */
     public static SplunkRum getInstance() {
         if (INSTANCE == null) {
             throw new IllegalStateException("SplunkRum has not been initialized.");
