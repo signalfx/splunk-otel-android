d404d082de28b6a1f8bdcb2680beb02ebf814347
jkwatson <jwatson@splunk.com>
2021-05-20 10:22:12 -0700

    first cut at fragment tracking. tests needed

--- splunk-otel-android/src/main/java/com/splunk/rum/RumLifecycleCallbacks.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/RumLifecycleCallbacks.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,9 +15,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import io.opentelemetry.api.trace.Tracer;
 
 class RumLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
-    private static final NoOpTracer NO_OP_TRACER = new NoOpTracer();
 
-    private final Map<String, ActivityTracer> tracersByActivityClassName = new HashMap<>();
+    private final Map<String, NamedTrackableTracer> tracersByActivityClassName = new HashMap<>();
     private final AtomicBoolean appStartupComplete = new AtomicBoolean();
     private final Tracer tracer;
 
@@ -27,8 +27,13 @@ class RumLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onActivityPreCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
         getOrCreateTracer(activity)
-                .startActivityCreation()
+                .startTrackableCreation()
                 .addEvent("activityPreCreated");
+
+        if (activity instanceof AppCompatActivity) {
+            ((AppCompatActivity) activity).getSupportFragmentManager()
+                    .registerFragmentLifecycleCallbacks(new RumFragmentLifecycleCallbacks(tracer), true);
+        }
     }
 
     @Override
@@ -148,45 +153,20 @@ class RumLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
     }
 
     private TrackableTracer getOrCreateTracer(Activity activity) {
-        ActivityTracer activityTracer = tracersByActivityClassName.get(activity.getClass().getName());
+        NamedTrackableTracer activityTracer = tracersByActivityClassName.get(activity.getClass().getName());
         if (activityTracer == null) {
-            activityTracer = new ActivityTracer(activity, appStartupComplete, tracer);
+            activityTracer = new NamedTrackableTracer(activity, appStartupComplete, tracer);
             tracersByActivityClassName.put(activity.getClass().getName(), activityTracer);
         }
         return activityTracer;
     }
 
     private TrackableTracer getActivityTracer(@NonNull Activity activity) {
-        ActivityTracer activityTracer = tracersByActivityClassName.get(activity.getClass().getName());
+        NamedTrackableTracer activityTracer = tracersByActivityClassName.get(activity.getClass().getName());
         if (activityTracer == null) {
-            return NO_OP_TRACER;
+            return TrackableTracer.NO_OP_TRACER;
         }
         return activityTracer;
     }
 
-    private static class NoOpTracer implements TrackableTracer {
-
-        @Override
-        public TrackableTracer startSpanIfNoneInProgress(String action) {
-            return this;
-        }
-
-        @Override
-        public TrackableTracer startActivityCreation() {
-            return this;
-        }
-
-        @Override
-        public void endSpanForActivityResumed() {
-        }
-
-        @Override
-        public void endActiveSpan() {
-        }
-
-        @Override
-        public TrackableTracer addEvent(String eventName) {
-            return this;
-        }
-    }
 }
