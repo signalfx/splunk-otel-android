66c19be22e86b9a200a768f8008b56acc7aee074
jkwatson <jwatson@splunk.com>
2021-05-20 08:05:53 -0700

    introduce a more fluent activity tracing API

--- splunk-otel-android/src/main/java/com/splunk/rum/ActivityTracer.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/ActivityTracer.java
@@ -9,7 +9,7 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
 
-class ActivityTracer {
+class ActivityTracer implements TrackableTracer {
     static final AttributeKey<String> ACTIVITY_NAME_KEY = AttributeKey.stringKey("activityName");
 
     private final Activity activity;
@@ -25,14 +25,15 @@ class ActivityTracer {
         this.tracer = tracer;
     }
 
-    public void startSpanIfNoneInProgress(String action) {
+    public ActivityTracer startSpanIfNoneInProgress(String action) {
         if (span != null) {
-            return;
+            return this;
         }
         startSpan(activity.getClass().getSimpleName() + " " + action);
+        return this;
     }
 
-    void startActivityCreation() {
+    public ActivityTracer startActivityCreation() {
         String spanName;
         //If the application has never loaded an activity, we name this span specially to show that
         //it's the application starting up. Otherwise, use the activity class name as the base of the span name.
@@ -42,6 +43,7 @@ class ActivityTracer {
             spanName = activity.getClass().getSimpleName() + " Created";
         }
         startSpan(spanName);
+        return this;
     }
 
     private void startSpan(String spanName) {
@@ -51,12 +53,12 @@ class ActivityTracer {
         scope = span.makeCurrent();
     }
 
-    void endActivityResumtionSpan() {
+    public void endSpanForActivityResumed() {
         appStartupComplete.set(true);
         endActiveSpan();
     }
 
-    void endActiveSpan() {
+    public void endActiveSpan() {
         if (scope != null) {
             scope.close();
             scope = null;
@@ -67,9 +69,10 @@ class ActivityTracer {
         }
     }
 
-    void addEvent(String eventName) {
+    public ActivityTracer addEvent(String eventName) {
         if (span != null) {
             span.addEvent(eventName);
         }
+        return this;
     }
 }
