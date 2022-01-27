f7acaaffbddc4fb80ff3e30b18cb72cef4ce4ba6
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-27 17:33:16 +0100

    Enable gradle enterprise
    And GHA build concurrency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,15 @@
 rootProject.name = "Splunk Android RUM"
 
+plugins {
+    id("com.gradle.enterprise") version "3.8.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 include(":splunk-otel-android")
 include(":sample-app")
