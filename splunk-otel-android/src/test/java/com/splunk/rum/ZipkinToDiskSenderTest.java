fa05de38229106b9ddaa64888db029d118e95622
Jason Plumb <jplumb@splunk.com>
2022-11-14 12:51:33 -0800

    guard against creating empty spans files and delete them gracefully on the exporter side.

--- splunk-otel-android/src/test/java/com/splunk/rum/ZipkinToDiskSenderTest.java
+++ splunk-otel-android/src/test/java/com/splunk/rum/ZipkinToDiskSenderTest.java
@@ -17,15 +17,20 @@
 package com.splunk.rum;
 
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+
+import static java.util.Collections.emptyList;
 
 import io.opentelemetry.sdk.common.Clock;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -51,8 +56,8 @@ class ZipkinToDiskSenderTest {
 
     @BeforeEach
     void setup() {
-        when(clock.now()).thenReturn(now);
-        when(limiter.ensureFreeSpace()).thenReturn(true);
+        lenient().when(clock.now()).thenReturn(now);
+        lenient().when(limiter.ensureFreeSpace()).thenReturn(true);
     }
 
     @Test
@@ -68,6 +73,18 @@ class ZipkinToDiskSenderTest {
         sender.sendSpans(spans);
 
         verify(fileUtils).writeAsLines(finalPath, spans);
+    }
+
+    @Test
+    void testEmptyListDoesNotWriteFile() {
+        ZipkinToDiskSender sender =
+                ZipkinToDiskSender.builder()
+                        .path(path)
+                        .fileUtils(fileUtils)
+                        .storageLimiter(limiter)
+                        .build();
+        sender.sendSpans(emptyList());
+        verifyNoInteractions(fileUtils);
     }
 
     @Test
