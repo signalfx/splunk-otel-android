565e12cba816551b0ff9bb1fcc5467ebbd15a841
Jason Plumb <jplumb@splunk.com>
2023-02-14 16:58:36 -0800

    add test

---
+++ splunk-otel-android/src/test/java/io/opentelemetry/rum/internal/instrumentation/activity/RumFragmentActivityRegistererTest.java
@@ -0,0 +1,74 @@
+package io.opentelemetry.rum.internal.instrumentation.activity;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import android.app.Activity;
+import android.app.Application;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RumFragmentActivityRegistererTest {
+
+    @Mock
+    FragmentManager.FragmentLifecycleCallbacks fragmentCallbacks;
+
+    @Test
+    void createHappyPath() {
+        FragmentActivity activity = mock(FragmentActivity.class);
+        FragmentManager manager = mock(FragmentManager.class);
+
+        when(activity.getSupportFragmentManager()).thenReturn(manager);
+
+        Application.ActivityLifecycleCallbacks underTest = RumFragmentActivityRegisterer.create(fragmentCallbacks);
+
+        underTest.onActivityPreCreated(activity, null);
+        verify(manager).registerFragmentLifecycleCallbacks(fragmentCallbacks, true);
+    }
+
+    @Test
+    void callbackIgnoresNonFragmentActivity() {
+        Activity activity = mock(Activity.class);
+        FragmentManager manager = mock(FragmentManager.class);
+
+        Application.ActivityLifecycleCallbacks underTest = RumFragmentActivityRegisterer.create(fragmentCallbacks);
+
+        underTest.onActivityPreCreated(activity, null);
+        verifyNoInteractions(manager);
+    }
+
+    @Test
+    void createPre29HappyPath() {
+        FragmentActivity activity = mock(FragmentActivity.class);
+        FragmentManager manager = mock(FragmentManager.class);
+
+        when(activity.getSupportFragmentManager()).thenReturn(manager);
+
+        Application.ActivityLifecycleCallbacks underTest = RumFragmentActivityRegisterer.createPre29(fragmentCallbacks);
+
+        underTest.onActivityCreated(activity, null);
+        verify(manager).registerFragmentLifecycleCallbacks(fragmentCallbacks, true);
+    }
+
+    @Test
+    void pre29CallbackIgnoresNonFragmentActivity() {
+        Activity activity = mock(Activity.class);
+        FragmentManager manager = mock(FragmentManager.class);
+
+        Application.ActivityLifecycleCallbacks underTest = RumFragmentActivityRegisterer.createPre29(fragmentCallbacks);
+
+        underTest.onActivityCreated(activity, null);
+        verifyNoInteractions(manager);
+    }
+
+
+}
