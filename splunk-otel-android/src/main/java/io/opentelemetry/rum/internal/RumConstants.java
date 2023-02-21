b6dd9bc7ea8574ae8307bcb9795e851ad7730c60
Jason Plumb <jplumb@splunk.com>
2023-02-21 12:59:09 -0800

    keep COMPONENT_KEY within splunk namespace (for now)

--- splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/RumConstants.java
+++ splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/RumConstants.java
@@ -24,15 +24,15 @@ public class RumConstants {
 
     public static final String OTEL_RUM_LOG_TAG = "OpenTelemetryRum";
 
-    public static final AttributeKey<String> COMPONENT_KEY = AttributeKey.stringKey("component");
     public static final AttributeKey<String> LAST_SCREEN_NAME_KEY =
             AttributeKey.stringKey("last.screen.name");
     public static final AttributeKey<String> SCREEN_NAME_KEY =
             AttributeKey.stringKey("screen.name");
     public static final AttributeKey<String> START_TYPE_KEY = stringKey("start.type");
 
-    public static final String COMPONENT_APPSTART = "appstart";
-    public static final String COMPONENT_UI = "ui";
+    public static final String APP_START_SPAN_NAME = "AppStart";
+
+    // chopping block
 
     private RumConstants() {}
 }
