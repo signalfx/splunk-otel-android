6e82ad99721efb20837654872dde738f31e7cda2
Jason Plumb <jplumb@splunk.com>
2023-02-24 12:05:14 -0800

    spotless

--- splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/instrumentation/lifecycle/AndroidLifecycleInstrumentation.java
+++ splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/instrumentation/lifecycle/AndroidLifecycleInstrumentation.java
@@ -122,5 +122,4 @@ public class AndroidLifecycleInstrumentation {
         }
         return new VisibleScreenLifecycleBinding(visibleScreenTracker);
     }
-
 }
--- splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/instrumentation/lifecycle/AndroidLifecycleInstrumentationBuilder.java
+++ splunk-otel-android/src/main/java/io/opentelemetry/rum/internal/instrumentation/lifecycle/AndroidLifecycleInstrumentationBuilder.java
@@ -1,10 +1,25 @@
-package io.opentelemetry.rum.internal.instrumentation.lifecycle;
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.function.Function;
+package io.opentelemetry.rum.internal.instrumentation.lifecycle;
 
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.rum.internal.instrumentation.activity.VisibleScreenTracker;
 import io.opentelemetry.rum.internal.instrumentation.startup.AppStartupTimer;
+import java.util.function.Function;
 
 public class AndroidLifecycleInstrumentationBuilder {
     AppStartupTimer startupTimer;
@@ -16,12 +31,14 @@ public class AndroidLifecycleInstrumentationBuilder {
         return this;
     }
 
-    public AndroidLifecycleInstrumentationBuilder setVisibleScreenTracker(VisibleScreenTracker tracker) {
+    public AndroidLifecycleInstrumentationBuilder setVisibleScreenTracker(
+            VisibleScreenTracker tracker) {
         this.visibleScreenTracker = tracker;
         return this;
     }
 
-    public AndroidLifecycleInstrumentationBuilder setTracerCustomizer(Function<Tracer, Tracer> customizer) {
+    public AndroidLifecycleInstrumentationBuilder setTracerCustomizer(
+            Function<Tracer, Tracer> customizer) {
         this.tracerCustomizer = customizer;
         return this;
     }
