eab6d485f3d09a6358fc46ebf376a890517d23ec
Jason Plumb <jplumb@splunk.com>
2023-03-23 14:20:51 -0700

    Make default attribute "rum.session.id".

--- splunk-otel-android/src/test/java/io/opentelemetry/rum/internal/SessionIdSpanAppenderTest.java
+++ splunk-otel-android/src/test/java/io/opentelemetry/rum/internal/SessionIdSpanAppenderTest.java
@@ -21,6 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import static io.opentelemetry.rum.internal.RumConstants.SESSION_ID_KEY;
+
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import org.junit.jupiter.api.Test;
@@ -43,7 +45,7 @@ class SessionIdSpanAppenderTest {
         assertTrue(underTest.isStartRequired());
         underTest.onStart(Context.root(), span);
 
-        verify(span).setAttribute(SessionIdSpanAppender.SESSION_ID_KEY, "42");
+        verify(span).setAttribute(SESSION_ID_KEY, "42");
 
         assertFalse(underTest.isEndRequired());
     }
