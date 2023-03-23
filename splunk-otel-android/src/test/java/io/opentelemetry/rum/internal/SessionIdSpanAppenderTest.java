9155220a1422bcf9e6259a06bc7857eba8bb9f32
Jason Plumb <jplumb@splunk.com>
2023-03-23 14:20:51 -0700

    Make default attribute "rum.session.id".

--- splunk-otel-android/src/test/java/io/opentelemetry/rum/internal/SessionIdSpanAppenderTest.java
+++ splunk-otel-android/src/test/java/io/opentelemetry/rum/internal/SessionIdSpanAppenderTest.java
@@ -22,6 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import static io.opentelemetry.rum.internal.RumConstants.SESSION_ID_KEY;
+
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import org.junit.jupiter.api.Test;
