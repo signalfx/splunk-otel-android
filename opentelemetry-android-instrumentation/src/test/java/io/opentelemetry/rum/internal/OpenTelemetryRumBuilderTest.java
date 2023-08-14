cf68a5608f96d97130743d2a5763925ee7322aa7
Surbhi Maheshwari <surbhim@splunk.com>
2023-08-14 13:45:17 -0700

    Fixed unit test

--- opentelemetry-android-instrumentation/src/test/java/io/opentelemetry/rum/internal/OpenTelemetryRumBuilderTest.java
+++ opentelemetry-android-instrumentation/src/test/java/io/opentelemetry/rum/internal/OpenTelemetryRumBuilderTest.java
@@ -16,6 +16,7 @@
 
 package io.opentelemetry.rum.internal;
 
+import static io.opentelemetry.rum.internal.RumConstants.RUM_SESSION_ID_KEY;
 import static io.opentelemetry.rum.internal.RumConstants.SESSION_ID_KEY;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.equalTo;
@@ -81,7 +82,9 @@ class OpenTelemetryRumBuilderTest {
         assertThat(spans.get(0))
                 .hasName("test span")
                 .hasResource(resource)
-                .hasAttributesSatisfyingExactly(equalTo(SESSION_ID_KEY, sessionId));
+                .hasAttributesSatisfyingExactly(
+                    equalTo(SESSION_ID_KEY, sessionId),
+                    equalTo(RUM_SESSION_ID_KEY, sessionId));
     }
 
     @Test
