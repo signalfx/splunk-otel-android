2a993406fbdd46037264f39a820b416d39740745
Surbhi Maheshwari <surbhim@splunk.com>
2023-08-25 14:25:20 -0700

    Made the constructor private

--- splunk-otel-android/src/main/java/com/splunk/rum/BackgroundProcessDetector.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/BackgroundProcessDetector.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 
 public final class BackgroundProcessDetector {
 
-    BackgroundProcessDetector() {}
+    private BackgroundProcessDetector() {}
 
     public static Boolean isBackgroundProcess(String applicationId) {
         String applicationProcessName = getApplicationProcessName();
