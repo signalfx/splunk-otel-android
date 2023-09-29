36da239ce8b54bc885ae437d5dc740479b4dcd0b
reza stallone <rezastallone@gmail.com>
2023-09-29 22:29:09 +0700

    add javadoc for SpanStorage
    Signed-off-by: reza stallone <rezastallone@gmail.com>

--- splunk-otel-android/src/main/java/com/splunk/rum/SpanStorage.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/SpanStorage.java
@@ -3,13 +3,30 @@ package com.splunk.rum;
 import java.io.File;
 import java.util.stream.Stream;
 
+/***
+ * Manage location for storing span, provide spans that can be sent to exporter
+ * or all spans stored in the location.
+ */
 interface SpanStorage {
 
+    /***
+     * Provide location of storing spans
+     * @return
+     */
     File provideSpanFile();
 
+    /***
+     * @return all spans including those that can be sent or not
+     */
     Stream<File> getAllSpanFiles();
 
+    /***
+     * @return total size of all span that can be sent or not
+     */
     long getTotalFileSizeInBytes();
 
+    /***
+     * @return all spans that can be sent
+     */
     Stream<File> getPendingFiles();
 }
