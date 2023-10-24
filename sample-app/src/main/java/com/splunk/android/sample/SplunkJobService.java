1660b1b72daf1e97fb8e551dc65553accf4b8669
Surbhi Maheshwari <surbhim@splunk.com>
2023-10-24 14:12:22 -0700

    Fix spotless

--- sample-app/src/main/java/com/splunk/android/sample/SplunkJobService.java
+++ sample-app/src/main/java/com/splunk/android/sample/SplunkJobService.java
@@ -1,13 +1,26 @@
-package com.splunk.android.sample;
+/*
+ * Copyright Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.splunk.android.sample;
 
 import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
-
 
 @SuppressLint("SpecifyJobSchedulerIdRange")
 public class SplunkJobService extends JobService {
@@ -17,16 +30,15 @@ public class SplunkJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
-//        Intent serviceIntent = new Intent(context, SplunkBackgroundService.class);
+        //        Intent serviceIntent = new Intent(context, SplunkBackgroundService.class);
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-//        Intent serviceIntent = new Intent(context, SplunkBackgroundService.class);
-//        context.stopService(serviceIntent);
+        //        Intent serviceIntent = new Intent(context, SplunkBackgroundService.class);
+        //        context.stopService(serviceIntent);
         Log.d(TAG, "Job paused.");
         return true;
     }
-
 }
