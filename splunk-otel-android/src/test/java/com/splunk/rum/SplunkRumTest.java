d3caa2b15f1b40823160fd360caf337c957a5e49
Jason Plumb <jplumb@splunk.com>
2023-11-02 14:28:37 -0700

    spotless

--- splunk-otel-android/src/test/java/com/splunk/rum/SplunkRumTest.java
+++ splunk-otel-android/src/test/java/com/splunk/rum/SplunkRumTest.java
@@ -24,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
