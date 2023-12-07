b12405b1ff58d86da9edc1bd80c5f2693f06ad37
Jason Plumb <jplumb@splunk.com>
2023-12-07 15:05:16 -0800

    migrate deprecated buildConfig property and fix build

--- splunk-otel-android-volley/build.gradle.kts
+++ splunk-otel-android-volley/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     compileOptions {
         sourceCompatibility(JavaVersion.VERSION_1_8)
         targetCompatibility(JavaVersion.VERSION_1_8)
+        isCoreLibraryDesugaringEnabled = true
     }
 
     testOptions {
@@ -51,6 +52,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-fragment:2.7.5")
     compileOnly("com.android.volley:volley:1.2.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation(project(":splunk-otel-android"))
 
