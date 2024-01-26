415b880829a26d1e7cbfa12dc35a94d18d581632
Jason Plumb <jplumb@splunk.com>
2024-01-26 14:45:01 -0800

    specify zipkin sender version

--- splunk-otel-android/build.gradle.kts
+++ splunk-otel-android/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
 
     api("io.opentelemetry:opentelemetry-api")
     api("com.squareup.okhttp3:okhttp:4.12.0")
-    api("io.zipkin.reporter2:zipkin-sender-okhttp3")
+    api("io.zipkin.reporter2:zipkin-sender-okhttp3:2.17.2")
 
     testImplementation("org.mockito:mockito-core:5.8.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.8.0")
