8d9a7fcaadd24db1b302b8e571e7e267e122d22b
Jason Plumb <jplumb@splunk.com>
2024-02-07 13:17:02 -0800

    update to new URLs

--- sample-app/src/main/java/com/splunk/android/sample/ShopWebViewFragment.java
+++ sample-app/src/main/java/com/splunk/android/sample/ShopWebViewFragment.java
@@ -51,7 +51,7 @@ public class ShopWebViewFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        String storeUrl = "https://frontend-us.o11ystore.com/";
+        String storeUrl = "https://frontend-us.splunko11y.com/";
         binding.webView.setWebViewClient(
                 new WebViewClientCompat() {
                     @Override
