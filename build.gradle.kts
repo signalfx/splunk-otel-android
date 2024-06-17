765155dd51a3f234a81f0bc1a08eb7c5b354ea3a
Jason Plumb <jplumb@splunk.com>
2024-06-17 10:16:55 -0700

    update sonatype env vars to match other repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ subprojects {
 
 nexusPublishing.repositories {
     sonatype {
-        username.set(System.getenv("SONATYPE_USERNAME"))
-        password.set(System.getenv("SONATYPE_PASSWORD"))
+        username.set(System.getenv("SONATYPE_USER"))
+        password.set(System.getenv("SONATYPE_KEY"))
     }
 }
