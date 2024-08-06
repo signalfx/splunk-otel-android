16bcbb5b56e381072491bdd55e2b6123f2ea6342
Jason Plumb <jplumb@splunk.com>
2024-08-05 21:14:06 -0700

    prevent upstream default exporter from being configured (for now).

---
+++ splunk-otel-android/src/main/java/com/splunk/rum/internal/NoOpSpanExporter.java
@@ -0,0 +1,28 @@
+package com.splunk.rum.internal;
+
+import java.util.Collection;
+
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+
+/*
+ * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
+ * at any time.
+ */
+public final class NoOpSpanExporter implements SpanExporter {
+    @Override
+    public CompletableResultCode export(Collection<SpanData> spans) {
+        return CompletableResultCode.ofSuccess();
+    }
+
+    @Override
+    public CompletableResultCode flush() {
+        return CompletableResultCode.ofSuccess();
+    }
+
+    @Override
+    public CompletableResultCode shutdown() {
+        return CompletableResultCode.ofSuccess();
+    }
+}
