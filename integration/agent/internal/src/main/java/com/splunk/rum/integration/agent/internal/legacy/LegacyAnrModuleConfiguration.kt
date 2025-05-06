f22d3bbe8ce4936637d6eecb843fe0413d450a2f
Zdeněk Vaníček <zdenda@cisco.com>
2025-05-06 15:55:01 +0200

    Fix lint after merge

--- integration/agent/internal/src/main/java/com/splunk/rum/integration/agent/internal/legacy/LegacyAnrModuleConfiguration.kt
+++ integration/agent/internal/src/main/java/com/splunk/rum/integration/agent/internal/legacy/LegacyAnrModuleConfiguration.kt
@@ -19,7 +19,7 @@ package com.splunk.rum.integration.agent.internal.legacy
 import com.splunk.rum.integration.agent.module.ModuleConfiguration
 
 @Deprecated("Only to support legacy API, can be removed with legacy API.")
-class LegacyAnrModuleConfiguration(val isEnabled: Boolean = true)  : ModuleConfiguration {
+class LegacyAnrModuleConfiguration(val isEnabled: Boolean = true) : ModuleConfiguration {
 
     override val name: String = "anr"
 
