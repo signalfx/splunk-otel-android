2a883d4d465ef72c5e3ecdf7086059924207b28b
Tomas Chladek <tchladek@splunk.com>
2025-05-23 10:14:49 +0200

    Added license header
    Signed-off-by: Tomas Chladek <tchladek@splunk.com>

--- integration/agent/api/src/main/java/com/splunk/rum/integration/agent/api/attributes/StandardAttributes.kt
+++ integration/agent/api/src/main/java/com/splunk/rum/integration/agent/api/attributes/StandardAttributes.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.splunk.rum.integration.agent.api.attributes
 
 import io.opentelemetry.android.export.SpanDataModifier
@@ -25,8 +41,6 @@ object StandardAttributes {
     /**
      * Full HTTP client request URL in the form `scheme://host[:port]/path?query[#fragment]`.
      * Useful for span data filtering with the [SpanDataModifier].
-     *
-     * @see SemanticAttributes.HTTP_URL
      */
     @JvmStatic
     val HTTP_URL: AttributeKey<String> = AttributeKey.stringKey("http.url")
