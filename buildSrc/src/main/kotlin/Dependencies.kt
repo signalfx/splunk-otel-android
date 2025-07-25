48940d7cc2d3955252f39fb2f7ec4d02a0fc6d90
Tomáš Chládek <97025213+TomasChladekSL@users.noreply.github.com>
2025-07-25 09:37:38 +0200

    Update session replay (#1353)
    Signed-off-by: Tomas Chladek <tchladek@splunk.com>

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -87,7 +87,7 @@ object Dependencies {
     }
 
     object SessionReplay {
-        private const val version = "1.0.15"
+        private const val version = "1.0.16"
 
         const val bridge = "com.cisco.android:sr-bridge:$version"
         const val commonEncoder = "com.cisco.android:sr-common-encoder:$version"
