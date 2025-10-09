0dce098d0d8d1816d65e4f6a787cd1c2f1ec62db
Tony <tonzhan2@cisco.com>
2025-10-08 17:03:47 -0700

    add all changes

--- instrumentation/buildtime/mapping-file/plugin/build.gradle.kts
+++ instrumentation/buildtime/mapping-file/plugin/build.gradle.kts
@@ -13,27 +13,7 @@ plugins {
 group = defaultGroupId
 version = Configurations.sdkVersionName
 
-val javadocJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("javadoc")
-    from(tasks.named("javadoc"))
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
-
-tasks.jar {
-    manifest {
-        attributes(
-            "Implementation-Version" to Dependencies.Otel.otelAndroidBomVersion
-        )
-    }
-}
-
-tasks.withType<PublishToMavenRepository>().configureEach {
-    dependsOn(tasks.withType<Sign>())
-}
+createStandardJars()
 
 gradlePlugin {
     plugins {
@@ -50,33 +30,6 @@ dependencies {
     implementation(Dependencies.gradle)
 }
 
-signing {
-    val signingKey: String? = project.findProperty("signingKey") as String?
-    val signingPassword: String? = project.findProperty("signingPassword") as String?
-
-    if (signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications)
-    } else {
-        println("WARNING: Environment variables signingKey and/or signingPassword not set. Skipping signing of artifacts.")
-    }
-}
-
-publishing {
-    publications {
-        withType(MavenPublication::class.java) {
-            pom.withXml { asNode().addSplunkInfo() }
-
-            artifactId = "${artifactPrefix}$pluginName"
-
-            artifact(javadocJar)
-            artifact(sourcesJar)
-        }
-        repositories {
-            maven {
-                name = "local"
-                url = uri("$projectDir/repo")
-            }
-        }
-    }
-}
+configureGradlePluginSigning()
+configureGradlePluginPublishing("${artifactPrefix}$pluginName")
+addPublishingOutput()
