2339633ac682884934c7fff5cd6252fa6e4dd6fc
Tony <tonzhan2@cisco.com>
2026-02-11 11:12:42 +1300

    addressed comments

--- integration/lifecycle/src/main/java/com/splunk/rum/integration/lifecycle/callback/ActivityLifecycleCallback.kt
+++ integration/lifecycle/src/main/java/com/splunk/rum/integration/lifecycle/callback/ActivityLifecycleCallback.kt
@@ -25,98 +25,101 @@ import com.splunk.rum.integration.lifecycle.model.LifecycleAction
 
 /**
  * Activity lifecycle callbacks for all API levels.
- * Pre/post callbacks (API 29+) will only be invoked by the system on API 29+.
  */
 internal class ActivityLifecycleCallback(private val emitter: LifecycleEventEmitter) :
     Application.ActivityLifecycleCallbacks {
 
+    private companion object {
+        const val TAG = "ActivityLifecycleCallback"
+    }
+
     override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPreCreated: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPreCreated: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_CREATED)
     }
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-        Logger.d("ActivityLifecycleCallback", "onActivityCreated: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityCreated: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.CREATED)
     }
 
     override fun onActivityPostCreated(activity: Activity, savedInstanceState: Bundle?) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostCreated: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostCreated: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_CREATED)
     }
 
     override fun onActivityPreStarted(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPreStarted: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPreStarted: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_STARTED)
     }
 
     override fun onActivityStarted(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityStarted: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityStarted: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.STARTED)
     }
 
     override fun onActivityPostStarted(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostStarted: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostStarted: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_STARTED)
     }
 
     override fun onActivityPreResumed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPreResumed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPreResumed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_RESUMED)
     }
 
     override fun onActivityResumed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityResumed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityResumed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.RESUMED)
     }
 
     override fun onActivityPostResumed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostResumed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostResumed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_RESUMED)
     }
 
     override fun onActivityPrePaused(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPrePaused: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPrePaused: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_PAUSED)
     }
 
     override fun onActivityPaused(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPaused: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPaused: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PAUSED)
     }
 
     override fun onActivityPostPaused(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostPaused: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostPaused: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_PAUSED)
     }
 
     override fun onActivityPreStopped(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPreStopped: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPreStopped: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_STOPPED)
     }
 
     override fun onActivityStopped(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityStopped: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityStopped: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.STOPPED)
     }
 
     override fun onActivityPostStopped(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostStopped: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostStopped: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_STOPPED)
     }
 
     override fun onActivityPreDestroyed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPreDestroyed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPreDestroyed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.PRE_DESTROYED)
     }
 
     override fun onActivityDestroyed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityDestroyed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityDestroyed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.DESTROYED)
     }
 
     override fun onActivityPostDestroyed(activity: Activity) {
-        Logger.d("ActivityLifecycleCallback", "onActivityPostDestroyed: ${activity::class.java.simpleName}")
+        Logger.d(TAG, "onActivityPostDestroyed: ${activity::class.java.simpleName}")
         emitter.emitActivityEvent(activity, LifecycleAction.POST_DESTROYED)
     }
 
