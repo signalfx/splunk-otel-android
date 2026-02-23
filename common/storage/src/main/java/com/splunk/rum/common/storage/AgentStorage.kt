1a45b98f6090afca0af806c9dd603c1012ac8e88
Ondrej Synak <osynak@splunk.com>
2026-02-23 10:09:00 +0100

    Fix the agent to preload storage in content provider.

--- common/storage/src/main/java/com/splunk/rum/common/storage/AgentStorage.kt
+++ common/storage/src/main/java/com/splunk/rum/common/storage/AgentStorage.kt
@@ -350,6 +350,7 @@ class AgentStorage(context: Context) : IAgentStorage {
         private const val SESSION_REPLAY_IDS_KEY = "BUFFERED_SESSION_REPLAY_IDS"
 
         private const val TAG = "AgentStorage"
+        private val lock = Any()
 
         /**
          * If storage model changes this version needs to be changed. This will ensure data consistency.
@@ -360,8 +361,14 @@ class AgentStorage(context: Context) : IAgentStorage {
         private var instance: IAgentStorage? = null
 
         fun attach(context: Context): IAgentStorage {
-            Logger.v(TAG, "attach(): AgentStorage attached.")
-            return instance ?: AgentStorage(context).also { instance = it }
+            instance?.let { return it }
+
+            return synchronized(lock) {
+                instance ?: AgentStorage(context).also {
+                    instance = it
+                    Logger.v(TAG, "attach(): AgentStorage attached.")
+                }
+            }
         }
     }
 }
