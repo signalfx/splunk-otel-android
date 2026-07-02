5a35b2abbf9f92858e4aa3fc41f0a9224ac9193d
Surbhi Agarwal <surbagar@cisco.com>
2026-07-01 17:25:10 -0700

    Update licenses

--- instrumentation/runtime/okhttp3-auto/src/test/kotlin/com/splunk/rum/instrumentation/okhttp3/auto/OkHttpInstrumentationTest.kt
+++ instrumentation/runtime/okhttp3-auto/src/test/kotlin/com/splunk/rum/instrumentation/okhttp3/auto/OkHttpInstrumentationTest.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright 2026 Splunk Inc.
+ * Copyright The OpenTelemetry Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
